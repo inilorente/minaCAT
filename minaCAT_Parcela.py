a1a3bc1a7dc1664a12967d4d145b8b6549eba79a
manubenitomoreno <manubenitomoreno@gmail.com>
2017-05-30 17:32:33 +0200

    Update minaCAT_Parcela.py

--- minaCAT_Parcela.py
+++ minaCAT_Parcela.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 
-archivocat= "C:\\Users\\02052\\Desktop\\CSV_CATASTRO\\CAT\\ALCORCON_2016-09-24.cat"
+archivocat= "E:\\EARTHLING\\12_minaCAT\\CAT\\11_27_U_2017-01-20.CAT"
 
 
 
@@ -24,7 +24,7 @@ archivocat= "C:\\Users\\02052\\Desktop\\CSV_CATASTRO\\CAT\\ALCORCON_2016-09-24.c
 
 print 'Leyendo CAT original...'
 
-df0 = pd.read_csv(archivocat, delimiter="··", header=None, encoding = 'ascii')
+df0 = pd.read_csv(archivocat, delimiter="··", header=None)
 
 df0['reg'] = df0[0].str.slice(0,2)
 
@@ -44,19 +44,19 @@ print 'Procesando Registro 11...'
 
 df11.is_copy = False
 
-df11['A_TREG'] = 11
+df11['AA_TREG'] = 11
 
-df11['B_PCAT'] = df11[0].str.slice(30,44)
+df11['AB_PCAT'] = df11[0].str.slice(30,44)
 
-df11['C_SUPP'] = df11[0].str.slice(295,305)
+df11['AC_SUPP'] = df11[0].str.slice(295,305)
 
-df11['D_SUPCT'] = df11[0].str.slice(305,312)
+df11['AD_SUPCT'] = df11[0].str.slice(305,312)
 
-df11['E_SUPCSR'] = df11[0].str.slice(312,319)
+df11['AE_SUPCSR'] = df11[0].str.slice(312,319)
 
-df11['F_SUPCBR'] = df11[0].str.slice(319,326)
+df11['AF_SUPCBR'] = df11[0].str.slice(319,326)
 
-df11['G_SUPCUB'] = df11[0].str.slice(326,333)
+df11['AG_SUPCUB'] = df11[0].str.slice(326,333)
 
 #Registro 14 Registro de Construcción. Existe uno por cada construcción de cada unidad constructiva en cada parcela catastral
 
@@ -64,17 +64,19 @@ print 'Procesando Registro 14...'
 
 df14.is_copy = False
 
-df14['A_TREG'] = 14
+df14['BA_TREG'] = 14
 
-df14['B_PCAT'] = df14[0].str.slice(30,44)
+df14['BB_PCAT'] = df14[0].str.slice(30,44)
 
-df14['C_NBF'] = df14[0].str.slice(50,54)
+df14['BC_NBF'] = df14[0].str.slice(50,54)
 
-df14['D_NUC'] = df14[0].str.slice(54,58)
+df14['BD_NUC'] = df14[0].str.slice(54,58)
 
-df14['E_SUPT'] = df14[0].str.slice(83,90)
+df14['BE_USO'] = df14[0].str.slice(70,73)
 
-df14['F_TIPOL'] = df14[0].str.slice(104,109)
+df14['BF_SUPT'] = df14[0].str.slice(83,90)
+
+df14['BG_TIPOL'] = df14[0].str.slice(104,109)
 
 #Registro 15 Registro de Bien Inmueble. Existirá uno por cada bien inmueble en cada parcela catastral.
 
@@ -82,30 +84,30 @@ print 'Procesando Registro 15...'
 
 df15.is_copy = False
 
-df15['A_TREG'] = 15
+df15['CA_TREG'] = 15
 
-df15['B_PCAT'] = df15[0].str.slice(30,44)
+df15['CB_PCAT'] = df15[0].str.slice(30,44)
 
-df15['C_NBF'] = df15[0].str.slice(44,48)
+df15['CC_NBF'] = df15[0].str.slice(44,48)
 
-df15['D_ANT'] = df15[0].str.slice(371,375)
+df15['CD_ANT'] = df15[0].str.slice(371,375)
 
-df15['E_USO'] = df15[0].str.slice(427,428)
+df15['CE_USO'] = df15[0].str.slice(427,428)
 
-df15['F_SUPCON'] = df15[0].str.slice(441,451)
+df15['CF_SUPCON'] = df15[0].str.slice(441,451)
 
-df15['G_SUPSOL'] = df15[0].str.slice(451,461)
+df15['CG_SUPSOL'] = df15[0].str.slice(451,461)
 
 
 # Archivos de salida  ##################################################################################
 
 print 'Escribiendo salida...'
 
-rutar11 = "C:\\Users\\02052\\Desktop\\CSV_CATASTRO\\CSV\\CAT_ALCORCON_REG11.csv" #Ruta del registro 11 entre comillas. NOTA: Introducir la extensión .csv#
+rutar11 = "E:\\EARTHLING\\12_minaCAT\\CSV_minacat\\CAT_CADIZ27_REG11.csv" #Ruta del registro 11 entre comillas. NOTA: Introducir la extensión .csv#
 
-rutar14 = "C:\\Users\\02052\\Desktop\\CSV_CATASTRO\\CSV\\CAT_ALCORCON_REG14.csv" #Ruta del registro 14 entre comillas. NOTA: Introducir la extensión .csv#
+rutar14 = "E:\\EARTHLING\\12_minaCAT\\CSV_minacat\\CAT_CADIZ27_REG14.csv" #Ruta del registro 14 entre comillas. NOTA: Introducir la extensión .csv#
 
-rutar15 = "C:\\Users\\02052\\Desktop\\CSV_CATASTRO\\CSV\\CAT_ALCORCON_REG15.csv" #Ruta del registro 15 entre comillas. NOTA: Introducir la extensión .csv#
+rutar15 = "E:\\EARTHLING\\12_minaCAT\\CSV_minacat\\CAT_CADIZ27_REG15.csv" #Ruta del registro 15 entre comillas. NOTA: Introducir la extensión .csv#
 
 
 
